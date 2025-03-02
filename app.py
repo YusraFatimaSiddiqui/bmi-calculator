45a0df42c6ccbd31d527062dfbc11cc6d0702d30
Yusra <yusrafatimasiddiqui@gmail.com>
2025-03-03 00:56:46 +0500

    new changes

--- app.py
+++ app.py
@@ -14,7 +14,29 @@ st.header("")
 st.subheader("This is a subheader")
 
 
+# from PIL import Image
+# img = Image.open("streamlit.png")
+# st.image(img, width=400)
 
+# btn = st.button("Increment")
+# count = 0
+# count = st.session_state.count =0
+
+
+# if "count" not in st.session_state:
+#     st.session_state.count =0
+# if btn:
+#     st.session_state.count +=1
+#     st.write(btn)
+#     st.write(f"button clicked  {st.session_state.count} time")
+# else :
+#     st.write(f"button not clicked {count} time")
+
+#chat_input = st.chat_input("Enter your prompt")
+#if chat_input:
+  #  st.chat_message("user").write(chat_input)
+ #   st.chat_message("assistant").write("how can i help you today?")
+#    st.write(chat_input)
 # Text
 st.text("Hello this is text")
 
--- app2.py
+++ app2.py
@@ -1,29 +1,4 @@
 import streamlit as st
-# from PIL import Image
-# img = Image.open("streamlit.png")
-# st.image(img, width=400)
-
-# btn = st.button("Increment")
-# count = 0
-# count = st.session_state.count =0
-
-
-# if "count" not in st.session_state:
-#     st.session_state.count =0
-# if btn:
-#     st.session_state.count +=1
-#     st.write(btn)
-#     st.write(f"button clicked  {st.session_state.count} time")
-# else :
-#     st.write(f"button not clicked {count} time")
-
-#chat_input = st.chat_input("Enter your prompt")
-#if chat_input:
-  #  st.chat_message("user").write(chat_input)
- #   st.chat_message("assistant").write("how can i help you today?")
-#    st.write(chat_input)
-
-import streamlit as st
 import pandas as pd
 
 st.title("BMI Calculator")
@@ -40,5 +15,3 @@ st.write("- Underweight: BMI less than 18.5")
 st.write("- Normal weight: BMI between 18.5 and 24.9")
 st.write("- Overweight: BMI between 25 and 29.9")
 st.write("- Obesity: BMI 30 or greater")
-
-
